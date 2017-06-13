48d6778b4cae5190e592e81f870365e9fa954e8d
Joseph Mallari <joseph.mallari@codeandtheory.com>
2017-06-13 14:14:06 -0400

    fix cors

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
 const SpotifyWebApi = require('spotify-web-api-node');
@@ -12,9 +11,13 @@ const spotifyApi = new SpotifyWebApi({
 	clientSecret: clientSecret
 });
 
-app.use(cors());
-
 app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {} );
 
