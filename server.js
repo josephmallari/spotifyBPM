96d4e400b39296aee9c2590752497000e40cc6c7
Joseph Mallari <joseph.mallari@codeandtheory.com>
2017-06-13 14:15:35 -0400

    cors

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ const spotifyApi = new SpotifyWebApi({
 	clientSecret: clientSecret
 });
 
-app.use(express.static('public'));
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {} );
 
