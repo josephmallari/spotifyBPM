1df53393a52eed800a008333f74e1e0732600101
Joseph Mallari <joseph.mallari@codeandtheory.com>
2017-06-13 12:44:18 -0400

    set up headers

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://spotify--bpm.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8081');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
