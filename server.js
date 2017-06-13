473a4b2c0f6ffb6f631f36d49471a13bc87bd84f
Joseph Mallari <joseph.mallari@codeandtheory.com>
2017-06-13 12:51:35 -0400

    add cors package

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const SpotifyWebApi = require('spotify-web-api-node');
@@ -11,27 +12,9 @@ const spotifyApi = new SpotifyWebApi({
 	clientSecret: clientSecret
 });
 
+app.use(cors());
+
 app.use(express.static('public'));
-
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8081');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 app.get('/', (req, res) => {} );
 
